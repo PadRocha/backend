119c7f8f92967844ea54968da24bb11dc51b9ddd
Padrón Rocha <p.roch@outlook.es>
2020-06-28 19:11:22 -0500

    FILE

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import helmet from "helmet";
 import path from "path";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
-import api from "./routes/api.routes";
 import config from "./config/config";
+import api from "./routes/api.routes";
 
 const app = express();
 
@@ -38,16 +38,14 @@ app.use(
   multer({
     storage,
     dest: path.join(__dirname, "uploads"),
-    fileFilter(req, file, cb: any) {
+    fileFilter(req, file, cb: Function) {
       var filetypes = /jpeg|jpg|png|gif/;
       var mimetype = filetypes.test(file.mimetype);
       var extname = filetypes.test(
         path.extname(file.originalname).toLowerCase(),
       );
       if (mimetype && extname) return cb(null, true);
-      cb(
-        `Error: File upload only supports the following filetypes - ${filetypes}`,
-      );
+      cb(`Error: File upload only supports the following filetypes - ${filetypes}`);
     },
     limits: { fileSize: 1000000 },
   }).single("image"),
