bb15715cf11c30dae636a5d0a8da982f47559e6e
Padrón Rocha <p.roch@outlook.es>
2020-12-30 10:29:00 -0600

    30/12/20

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,22 +9,22 @@ export default {
         SECRET: process.env.SECRET_KEY || '//password',
         SALT: Number(process.env.SALT) || 11,
     },
-    DB: {
+    MONGO: {
         URI: process.env.MONGO_URI || 'mongodb://localhost/database',
         USER: process.env.MONGO_USER,
-        PASSWORD: process.env.MONGO_PASSWORD
+        PASSWORD: process.env.MONGO_PASSWORD,
     },
     AUTH: {
         READ: Number(process.env.AUTH_READ) || 2,
         WRITE: Number(process.env.AUTH_WRITE) || 4,
         EDIT: Number(process.env.AUTH_EDIT) || 8,
         GRANT: Number(process.env.AUTH_GRANT) || 16,
-        ADMIN: Number(process.env.AUTH_ADMIN) || 32
+        ADMIN: Number(process.env.AUTH_ADMIN) || 32,
     },
     // CLOUDINARY: {
     //     ENV_VAR: process.env.CLOUDINARY_ENV_VAR,
     //     NAME: process.env.CLOUDINARY_NAME,
     //     KEY: process.env.CLOUDINARY_KEY,
-    //     SECRET: process.env.CLOUDINARY_SECRET
-    // }
+    //     SECRET: process.env.CLOUDINARY_SECRET,
+    // },
 }
--- src/database.ts
+++ src/database.ts
@@ -8,11 +8,36 @@ const dbOptions: ConnectionOptions = {
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-    config: { autoIndex: false }
-}
+    config: { autoIndex: false },
+};
 
-mongoose.connect(config.DB.URI, dbOptions, err => {
-    if (err) dump(err, '\x1b[37mDB: \x1b[31merror\x1b[0m');
-    else field('\x1b[37mDB', '\x1b[33mconnected\x1b[0m');
-    sep();
-});
+(() => {
+    //*------------------------------------------------------------------*/
+    // * Create the database connection 
+    //*------------------------------------------------------------------*/
+
+    mongoose.connect(config.MONGO.URI, dbOptions);
+
+    mongoose.connection.on('connected', () => {
+        field('\x1b[37mDatabase', `\x1b[33m${mongoose.connection.name}\x1b[0m`);
+        sep();
+    });
+
+    mongoose.connection.on('error', err => {
+        dump(err.name, '\x1b[37mDatabase[\x1b[31merror\x1b[0m]');
+        sep();
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Mongoose default connection disconnected');
+        sep();
+    });
+
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            console.log('Mongoose default connection disconnected through app termination');
+            sep();
+            process.exit(0);
+        });
+    });
+})();
